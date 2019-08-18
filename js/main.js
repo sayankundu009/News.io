1c398a7e91a28b429ebc5d0df881306f951fb096
Sayan Kundu <42407553+sayankundu9699@users.noreply.github.com>
2019-08-18 17:18:44 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+//Using NEWS API....
+//API KEY = 6c11a9dcc37449faa17fe15c4ebe377c
+
+let content = document.getElementById('news-content');
+
+let url =
+  'https://newsapi.org/v2/top-headlines?' +
+  'country=in&' +
+  'apiKey=6c11a9dcc37449faa17fe15c4ebe377c';
+
+let req = new Request(url);
+fetch(req)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(data => {
+    display_news(data.articles);
+  });
+
+let display_news = news => {
+  console.log(news);
+  news.forEach(article => {
+    if (article.author != null) {
+      content.innerHTML += `<div class="card mb-3">
+                                <img
+                                    class="card-img-top"
+                                    src="${article.urlToImage}"
+                                    alt="No img"
+                                />
+                                <div class="card-body">
+                                    <h5 class="card-title">${article.title}</h5>
+                                    <p class="card-text">
+                                        ${article.content}
+                                       
+                                    </p>
+                                    <a href="${
+                                      article.url
+                                    }" class="btn btn-primary mb-2">Read More</a>
+                                    <p class="card-text">
+                                    <small class="text-muted">
+                                  
+                                    </small>
+                                    </p>
+                                </div>
+                                </div>`;
+    }
+  });
+
+  show();
+};
+
+let show = () => {
+  document.querySelector('body').style.display = 'block';
+};
